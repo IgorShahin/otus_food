57d71696be4c68395f6ee244b0b8a4261a33df15
Igor Shahin <igor.shahin@icloud.com>
2024-06-23 02:39:55 +0300

    [feature] Added assets

---
+++ lib/core/res/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  AppAssets._();
+
+  static const iconFailure = 'assets/images/icon_failure.svg';
+  static const iconRecipe = 'assets/images/icon_recipe.png';
+  static const iconProfile = 'assets/images/icon_profile.png';
+}
