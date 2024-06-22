d438bbac925a1469892a7e462b8aca0e51a27b5b
Igor Shahin <igor.shahin@icloud.com>
2024-06-23 02:37:33 +0300

    [update] card recipe

--- lib/presentation/recipes/widgets/card_recipe.dart
+++ lib/presentation/recipes/widgets/card_recipe.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:otus_food/core/extensions/context_extension.dart';
-import 'package:otus_food/core/res/app_colors.dart';
 
 class CardRecipe extends StatelessWidget {
   const CardRecipe({
@@ -19,61 +18,64 @@ class CardRecipe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(5),
-      child: Ink(
-        padding: const EdgeInsets.only(right: 23),
-        width: 396,
-        height: 136,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: AppColors.shadow.withAlpha(10),
-                blurRadius: 4,
-                offset: const Offset(0, 4),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        onTap: onTap,
+        child: Ink(
+          color: Colors.transparent,
+          child: Row(
+            children: [
+              CachedNetworkImage(
+                imageUrl: image,
+                width: 149,
+                height: 136,
+                fit: BoxFit.cover,
+                imageBuilder: (context, imageProvider) {
+                  return Ink.image(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  );
+                },
               ),
-            ]),
-        child: Row(
-          children: [
-            CachedNetworkImage(
-                imageUrl: image, width: 149, height: 136, fit: BoxFit.cover),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    name,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Row(
-                    children: [
-                      const Icon(Icons.access_time_outlined, size: 21),
-                      const SizedBox(width: 11),
-                      Text(
-                        "$time min",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                          color: context.color.accentColor,
-                        ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      name,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        const Icon(Icons.access_time_outlined, size: 21),
+                        const SizedBox(width: 11),
+                        Text(
+                          "$time min",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: context.color.accentColor,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
