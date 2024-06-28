b8a36f9ee3e41ec6b9f5e58c689deab8351b19b1
Igor Shahin <igor.shahin@icloud.com>
2024-06-28 16:35:50 +0300

    [update] app colors

--- lib/core/res/app_colors.dart
+++ lib/core/res/app_colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const Color scaffoldBackground = Color(0xFFECECEC);
   static const Color hint = Color(0xFFC2C2C2);
   static const Color shadow = Color(0xFF959292);
+  static const Color base = Color(0xFFD9DED3);
 }
